3d3534ea8804c4ce5e2e9f167aa2ad4ebbb71bf9
Brunopbb <bruno.almeida@ee.ufcg.edu.br>
2025-06-14 13:54:54 -0300

    update

--- codigos-artigo/verification.py
+++ codigos-artigo/verification.py
@@ -3,10 +3,22 @@ import matplotlib.pyplot as plt
 
 data = np.load("/home/brunoee/Documents/dados/2025-05-10/Averages_0548.npz")
 
-data_ = np.zeros((1, 32768))
-data_[0] = np.fft.fftshift(data["054835"])
+aux_d = data["054835"].copy()
 
-print(data_)
+
+aux_d[25385:25502] += 1000
+aux_d[30273:30347] += 10000
+
+
+
+data_ = np.zeros((1, 32768))
+data_[0] = np.fft.fftshift(aux_d)
+
+
+
+aux = np.zeros((1, 32768))
+aux2 = np.concatenate((aux, data_), axis=0)
+
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
@@ -21,4 +33,11 @@ ax1.set_ylim(0, 5)
 ax2.set_ylim(ff, fi)
 ax1.set(xlabel="Frequency (MHz)", ylabel="Power (Not Calibrated)",
 title=f'Spectrum {fi}-{ff} MHz')
+
+cax = ax2.imshow(np.log10(data_.T + epsilon),
+                cmap='viridis', aspect='auto',
+                extent=[0, 200, ff, fi], vmin=0, vmax=5)
+plt.colorbar(cax, ax=ax2, label='Power (Not Calibrated)')
+
+
 plt.show()
